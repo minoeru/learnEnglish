f7f8c09af143434ca860b63090e497ca34a84f6b
minoeru <minoeru@users.noreply.github.com>
2019-08-20 10:40:58 +0900

    リファクタリング

--- word_test/app.R
+++ word_test/app.R
@@ -47,8 +47,8 @@ server <- function(input, output) {
     df_finished2 <<- as.character(df$deleted_word)
   })
   
+  ##################  単語画面  ##################  
   flag1 <- 0
-  question <- ""
   
   output$file_accept <- renderUI({
     return(list(
@@ -58,23 +58,14 @@ server <- function(input, output) {
     ))
   })
   
-  count_number <- function(){
+  countNumber <- function(){
     num <<- 0
-    for(i in 1:length(df_finished)){
-      if(df_finished[i] == ""){
-        num <<- num + 1
-      }
-    }
-    output$count <- renderText({
-      paste("残り",(length(df_finished) - num),"問です")
-    })
-    
-    if(num == length(df_finished)){
-      flag1 <<- 1
-    }
+    lapply(1:length(df_finished), function(x) if(df_finished[x] == "") num <<- num + 1)
+    output$count <- renderText({paste0("残り",(length(df_finished) - num),"問です")})
+    if(num == length(df_finished)) flag1 <<- 1
   }
   
-  create_word <- function(){
+  createWord <- function(){
     while(1){
       seed <<- as.integer( runif(1, min = 1, max = (length(df_english) + 1) ) )
       if(df_finished[seed] != ""){
@@ -84,7 +75,7 @@ server <- function(input, output) {
     }
   }
   
-  create_selection <- function(seed){
+  createSelection <- function(seed){
     rand <- as.integer(runif(100, min = 1, max = (length(df_english) + 1) ) ) 
     data <- c(seed,rand)
     data <- unique(data)
@@ -93,28 +84,18 @@ server <- function(input, output) {
     return(data)
   }
   
-  create_data <- function(){
-    count_number()
-    
+  createData <- function(){
+    countNumber()
     ans_num <<- as.numeric(input$select)
     
     if(flag1 == 0){
-      output$question <- renderUI({
-        question <- create_word()
-        return(h1(question))
-      })
-      output$input1 <- renderUI({
-        textInput("input_text", label = h3("Answer input"), value = "")
-      })
+      output$question <- renderUI({ return(h1(createWord())) })
+      output$input1 <- renderUI({ textInput("input_text", label = h3("Answer input"), value = "") })
       output$selections <- renderUI({
-        selection <- create_selection(seed)
-        return(lapply(1:ans_num,function(x){
-          tmp_name = x
-          actionButton("tmp_name",df_japanese[selection[x]],width = "100%")
-        }))
+        selection <- createSelection(seed)
+        return(lapply(1:ans_num,function(x)actionButton("x",df_japanese[selection[x]],width = "100%")))
       })
-    }
-    else{
+    }else{
       output$question <- renderUI({})
       output$input1 <- renderUI({})
       output$selections <- renderUI({})
@@ -122,48 +103,29 @@ server <- function(input, output) {
     }
   }
   
-  check_data <- function(){
-    ans <- input$input_text
-    if(ans == df_japanese[seed]){
-      create_data()
-    }
-  }
   
   observeEvent(input$start1,{
     output$file_accept <- renderUI({})
-    output$button <- renderUI({
-      actionButton("answer","Answer")
-    })
-    create_data()
+    output$button <- renderUI({ actionButton("answer","Answer") })
+    createData()
   })
   
   observeEvent(input$answer,{
-    check_data()
+    if(input$input_text == df_japanese[seed]) createData()
   })
   
-  # 例文
+  ##################  例文画面   ##################  
   
   flag2 <- 0
-  question2 <- ""
   
-  
-  count_number2 <- function(){
+  countNumber2 <- function(){
     num2 <<- 0
-    for(i in 1:length(df_finished2)){
-      if(df_finished2[i] == ""){
-        num2 <<- num2 + 1
-      }
-    }
-    output$count2 <- renderText({
-      paste("残り",(length(df_finished2) - num2),"問です")
-    })
-    
-    if(num2 == length(df_finished2)){
-      flag2 <<- 1
-    }
+    lapply(1:length(df_finished2),function(x) if(df_finished2[x] == "") num2 <<- num2 + 1)
+    output$count2 <- renderText({paste("残り",(length(df_finished2) - num2),"問です")})
+    if(num2 == length(df_finished2)) flag2 <<- 1
   }
   
-  create_word2 <- function(){
+  createWord2 <- function(){
     while(1){
       seed <<- as.integer( runif(1, min = 1, max = (length(changed_sentence) + 1) ) )
       if(df_finished2[seed] != ""){
@@ -173,7 +135,7 @@ server <- function(input, output) {
     }
   }
   
-  create_selection2 <- function(seed){
+  createSelection2 <- function(seed){
     rand <- as.integer(runif(100, min = 1, max = (length(changed_sentence) + 1) ) )
     data <- c(seed,rand)
     data <- unique(data)
@@ -182,25 +144,15 @@ server <- function(input, output) {
     return(data)
   }
   
-  create_data2 <- function(){
-    count_number2()
-    
+  createData2 <- function(){
+    countNumber2()
     ans_num2 <<- as.numeric(input$select2)
-    
     if(flag2 == 0){
-      output$question2 <- renderUI({
-        question2 <- create_word2()
-        return(h1(question2))
-      })
-      output$input2 <- renderUI({
-        textInput("input_text2", label = h3("Answer input"), value = "")
-      })
+      output$question2 <- renderUI({ return(h1(createWord2())) })
+      output$input2 <- renderUI({ textInput("input_text2", label = h3("Answer input"), value = "") })
       output$selections2 <- renderUI({
-        selection <- create_selection2(seed)
-        return(lapply(1:ans_num2, function(x){
-          tmp_name = x
-          actionButton("tmp_name",deleted_word[selection[x]],width = "100%")
-        }))
+        selection <- createSelection2(seed)
+        return(lapply(1:ans_num2, function(x) actionButton("x",deleted_word[selection[x]],width = "100%")))
       })
     }
     else{
@@ -208,13 +160,6 @@ server <- function(input, output) {
       output$input1 <- renderUI({})
       output$selections2 <- renderUI({})
       output$button2 <- renderUI({})
-    }
-  }
-  
-  check_data2 <- function(){
-    ans <- input$input_text2
-    if(ans == deleted_word[seed]){
-      create_data2()
     }
   }
   
@@ -231,13 +176,11 @@ server <- function(input, output) {
     output$button2 <- renderUI({
       actionButton("answer2","Answer")
     })
-    create_data2()
+    createData2()
   })
   
   observeEvent(input$answer2,{
-    check_data2()
+    if(input$input_text2 == deleted_word[seed]) createData2()
   })
-  
 }
-
 shinyApp(ui = ui, server = server)
